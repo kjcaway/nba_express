f0f4347a7ca60d8ec2cb744be82bc15f9626bbfe
kjcaway <whdckstngh@naver.com>
2019-01-13 15:55:27 +0900

    modify port

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const api = require('./routes/index');
 app.use(bodyParser.json());
 app.use('/api', api);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
