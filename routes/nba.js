e4657cc24389b5a95f153207a22485f5478b889e
kjcaway <whdckstngh@naver.com>
2019-01-13 14:42:51 +0900

    modify /player api

--- routes/nba.js
+++ routes/nba.js
@@ -5,9 +5,19 @@ const router = express.Router();
 
 router.post('/player', (req, res) => {
     const player = nba.findPlayer(req.body.name);
-    nba.stats.playerInfo({ 'PlayerID': player.playerId }).then(console.log)
-    
-    res.send(player)
+
+    if(player == null) {
+        return res.status(404).json({'error' : 1, 'message' : 'No match Player'})
+    }
+
+    nba.stats.playerInfo({ 'PlayerID': player.playerId })
+        .then((data) => {
+            return res.json({'error':0, 'data':data})
+        })
+        .catch(function (err) {
+            console.log(err);
+            return res.status(500).json({'error' : 1, 'message' : 'Error, playerInfo'})
+        });
 })
 
 module.exports = router
