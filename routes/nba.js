6316a57760c80dc0cd7c6e2fe46cc2b7bd079977
kjcaway <whdckstngh@naver.com>
2019-01-20 02:08:58 +0900

    add validate

--- routes/nba.js
+++ routes/nba.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const nba = require('nba');
+const Schema = require('validate');
 
 const router = express.Router();
 
+const playerSchema = new Schema({
+    "name": {
+        type :String,
+        required : true,
+        length:{min:3, max:32}
+    }
+})
+
 router.post('/player', (req, res) => {
+    const validError = playerSchema.validate(req.body)
+    if(validError.length > 0){
+        return res.status(400).json({'error': 1, 'message' :validError[0].message})
+    }
+
     const player = nba.findPlayer(req.body.name);
 
     if(player == null) {
-        return res.status(404).json({'error' : 1, 'message' : 'No match Player'})
+        console.log("No matching player.")
+        return res.status(204).json({'data' : null})
     }
 
+    // player id 로 추가 정보 조회 
     nba.stats.playerInfo({ 'PlayerID': player.playerId })
         .then((data) => {
-            return res.json({'error':0, 'data':data})
+            console.log("Find player. name == "+ data.commonPlayerInfo[0].displayFirstLast)
+            return res.json({'data':data})
         })
         .catch(function (err) {
             console.log(err);
